8f48f803ae84261752f27968d4c8f2d987acfd77
Nikita Makarin <n-makarin@mail.ru>
2019-12-05 18:27:09 +0300

    Add /users and /events routes to router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Events from "../views/Events.vue";
+import Users from "../views/Users.vue";
 
 Vue.use(VueRouter);
 
@@ -11,14 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function() {
-      return import(/* webpackChunkName: "about" */ "../views/About.vue");
-    }
+    path: "/users",
+    name: "users",
+    component: Users
+  },
+  {
+    path: "/events",
+    name: "events",
+    component: Events
   }
 ];
 
