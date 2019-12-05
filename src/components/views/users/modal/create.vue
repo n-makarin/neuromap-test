92bc54040671fb206d442b185675a0a1b7a2eb77
Nikita Makarin <n-makarin@mail.ru>
2019-12-05 22:32:18 +0300

    Create src\components\views\users\modal\create.vue

---
+++ src/components/views/users/modal/create.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="create" v-if="visible">
+    <form @submit.prevent="create">
+      <div
+        class="create__item create-item"
+        v-for="item in fieldList"
+        :key="item.id"
+      >
+        <label :for="item.id">{{ item.label }}</label>
+        <input :id="item.id" type="text" v-model="item.value" />
+      </div>
+      <button>create user</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      fieldList: [
+        {
+          id: "name",
+          value: "",
+          label: "name"
+        },
+        {
+          id: "surname",
+          value: "",
+          label: "surname"
+        }
+      ]
+    };
+  },
+  methods: {
+    create() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
