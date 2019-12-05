7754151d06498b44156c78ceefab4e713911017f
Nikita Makarin <n-makarin@mail.ru>
2019-12-05 20:33:08 +0300

    Add axios & sendRequest to src\main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import router from "./router";
+
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import sendRequest from "@/plugins/send-request.js";
 
 Vue.config.productionTip = false;
 
@@ -12,3 +18,6 @@ new Vue({
     return h(App);
   }
 }).$mount("#app");
+
+Vue.use(VueAxios, axios);
+Vue.use(sendRequest);
