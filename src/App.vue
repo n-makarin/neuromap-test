6a1ed798bb9096f52456d0d9690ddbef2ab37dec
Nikita Makarin <n-makarin@mail.ru>
2019-12-05 18:27:37 +0300

    Create src\components\layout\navigation.vue Add it to src\App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navigation from "@/components/layout/navigation.vue";
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/layout/navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navigation">
+    <router-link v-for="link in linkList" :key="link.title" :to="link.path">{{
+      link.title
+    }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      linkList: [
+        {
+          title: "users",
+          path: "users"
+        },
+        {
+          title: "events",
+          path: "events"
+        },
+        {
+          title: "home",
+          path: "/"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
