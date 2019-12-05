9048e58603d28609754770f4fce0fd96390ae539
Nikita Makarin <n-makarin@mail.ru>
2019-12-05 22:54:57 +0300

    Create src\components\views\users\modal\detail.vue

---
+++ src/components/views/users/modal/detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="detail" v-if="visible">
+    <div class="detail__name">{{ user.name }}</div>
+    <div class="detail__surname">{{ user.surname }}</div>
+    <button @click="$emit('close')">close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    user() {
+      return this.$store.getters["user/data"];
+    }
+  },
+  methods: {
+    close() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
