73723329a386a9afd78b740121d9310077ca4fbb
Nikita Makarin <n-makarin@mail.ru>
2019-12-06 10:19:09 +0300

    Add remove user action to src\store\user.js

--- src/store/user.js
+++ src/store/user.js
@@ -4,13 +4,15 @@ export default {
   namespaced: true,
   state: {
     data: {
+      id: 0,
       name: "",
       surname: ""
     }
   },
   mutations: {
-    SET(store, { name = "", surname = "" }) {
+    SET(store, { id = 0, name = "", surname = "" }) {
       store.data = {
+        id,
         name,
         surname
       };
@@ -23,13 +25,13 @@ export default {
      * @returns void
      */
     set({ commit }, user) {
-      if (!user.name || !user.surname) {
+      if (!user.id || !user.name || !user.surname) {
         return;
       }
       commit("SET", user);
     },
     /**
-     *
+     * Create user, add to store and db
      * @param {object} payload
      * @returns Promise<void>
      */
@@ -50,6 +52,28 @@ export default {
           const user = response.data;
           commit("SET", user);
           dispatch("user/list/add", user, { root: true });
+        });
+    },
+    /**
+     * Remove user from store and db
+     * @param {number} userId
+     * @returns Promise<void>
+     */
+    async remove({ commit, dispatch }, userId) {
+      await this._vm
+        .$sendRequest({
+          method: "DELETE",
+          url: `user/detail/${userId}`
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .then(response => {
+          if (!response.status === 200) {
+            return;
+          }
+          commit("SET", { id: 0, name: "", surname: "" });
+          dispatch("user/list/remove", userId, { root: true });
         });
     }
   },
