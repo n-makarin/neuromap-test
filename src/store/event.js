5f6acc9e70711c940f14004e6054f92beea55e00
Nikita Makarin <n-makarin@mail.ru>
2019-12-06 16:06:57 +0300

    Add setFacilitatorUser & setSecretaryUser actions Add mutations and getters src\store\event.js

--- src/store/event.js
+++ src/store/event.js
@@ -25,6 +25,12 @@ export default {
     },
     SET_PARTICIPANT_LIST(store, list) {
       store.participantList = list;
+    },
+    SET_FACILITATOR_LIST(store, user) {
+      store.facilitatorUser = user;
+    },
+    SET_SECRETARY_LIST(store, user) {
+      store.secretaryUser = user;
     }
   },
   actions: {
@@ -49,11 +55,34 @@ export default {
         list.push(user);
       }
       commit("SET_PARTICIPANT_LIST", list);
+    },
+    /**
+     * Get facilitator user by id from db and set to store
+     * @param {number} userId
+     * @returns Promise<void>
+     */
+    async setFacilitatorUser({ commit, dispatch }, userId) {
+      const user = await dispatch("user/get", userId, {
+        root: true
+      });
+      commit("SET_FACILITATOR_LIST", user);
+    },
+    /**
+     * Get secretary user by id from db and set to store
+     * @param {number} userId
+     * @returns Promise<void>
+     */
+    async setSecretaryUser({ commit, dispatch }, userId) {
+      const user = await dispatch("user/get", userId, {
+        root: true
+      });
+      commit("SET_SECRETARY_LIST", user);
     }
   },
   getters: {
     data: state => state.data,
     participantList: state => state.participantList,
+    facilitatorUser: state => state.facilitatorUser,
     secretaryUser: state => state.secretaryUser
   },
   modules: {
