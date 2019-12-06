b759e5539c01cf7706e90c61715e8b9660636ef9
Nikita Makarin <n-makarin@mail.ru>
2019-12-06 08:01:33 +0300

    Create src\components\layout\modal\default.vue

---
+++ src/components/layout/modal/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <portal to="root">
+    <div class="modal-default" v-if="visible">
+      <div class="modal-default__content">
+        <slot />
+      </div>
+      <div class="modal-default__overlay" @click="close"></div>
+    </div>
+  </portal>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
