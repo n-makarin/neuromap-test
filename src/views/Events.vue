7c36f4ec69be725a1bfba22c9ab8b44a01aaa909
Nikita Makarin <n-makarin@mail.ru>
2019-12-06 11:37:18 +0300

    Add list.vue, create.vue, detail.vue to Events.vue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="events">
-    <h1>events</h1>
+    <button @click="modal.create.visible = true">create event</button>
+    <list
+      :data="list"
+      title="event list"
+      @showDetail="modal.detail.visible = true"
+    />
+    <create
+      :visible="modal.create.visible"
+      @close="modal.create.visible = false"
+    />
+    <detail
+      :visible="modal.detail.visible"
+      @close="modal.detail.visible = false"
+    />
   </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import List from "@/components/views/events/list";
+import Create from "@/components/views/events/modal/create";
+import Detail from "@/components/views/events/modal/detail";
+
+export default {
+  components: {
+    List,
+    Create,
+    Detail
+  },
+  computed: {
+    ...mapGetters({
+      list: "event/list/data"
+    })
+  },
+  data() {
+    return {
+      modal: {
+        create: {
+          visible: false
+        },
+        detail: {
+          visible: false
+        }
+      }
+    };
+  },
+  methods: {
+    ...mapActions({
+      getList: "event/list/get"
+    })
+  },
+  mounted() {
+    // await while vm instance will be created
+    this.$nextTick(async () => {
+      await this.getList();
+    });
+  }
+};
+</script>
