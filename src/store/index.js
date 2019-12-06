7cf87eb6e639f4ba5e440b124f4ad8b31978ff54
Nikita Makarin <n-makarin@mail.ru>
2019-12-06 09:43:05 +0300

    Add vuex-shared-mutations to src\store\index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import user from "@/store/user.js";
+import sharedMutations from "vuex-shared-mutations";
 
 Vue.use(Vuex);
+
+const sharedMutationList = ["user/SET", "user/list/SET"];
 
 export default new Vuex.Store({
   namespaced: true,
@@ -11,5 +14,6 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     user
-  }
+  },
+  plugins: [sharedMutations({ predicate: sharedMutationList })]
 });
