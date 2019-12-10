6c5ea9eb429846e231f88005a799170cfa17bb2c
Nikita Makarin <n-makarin@mail.ru>
2019-12-10 14:46:59 +0300

    Add field computed props and markup src\components\views\events\modal\create.vue

--- src/components/views/events/modal/create.vue
+++ src/components/views/events/modal/create.vue
@@ -2,14 +2,45 @@
   <modal-default :visible="visible" @close="$emit('close')" size="md">
     <div class="create">
       <form @submit.prevent="create">
-        <div
-          class="create__item create-item"
-          v-for="item in fieldList"
-          :key="item.id"
-        >
-          <label :for="item.id">{{ item.label }}</label>
-          <input :id="item.id" type="text" v-model="item.value" />
-        </div>
+        <label :for="title.id">{{ title.label }}</label>
+        <input :id="title.id" type="text" v-model="title.value" />
+
+        <label :for="participantList.id">{{ participantList.label }}</label>
+        <multiselect
+          :id="participantList.id"
+          v-model="participantListValue"
+          :options="participantList.options"
+          :searchable="false"
+          placeholder="Pick a value"
+        />
+
+        <label :for="facilitatorUser.id">{{ facilitatorUser.label }}</label>
+        <multiselect
+          :id="facilitatorUser.id"
+          v-model="facilitatorUserValue"
+          :options="facilitatorUser.options"
+          :searchable="false"
+          placeholder="Pick a value"
+        />
+
+        <label :for="secretaryUser.id">{{ secretaryUser.label }}</label>
+        <multiselect
+          :id="secretaryUser.id"
+          v-model="secretaryUserValue"
+          :options="secretaryUser.options"
+          :searchable="false"
+          placeholder="Pick a value"
+        />
+
+        <label :for="secretaryUser.id">{{ secretaryUser.label }}</label>
+        <multiselect
+          :id="secretaryUser.id"
+          v-model="secretaryUserValue"
+          :options="secretaryUser.options"
+          :searchable="false"
+          placeholder="Pick a value"
+        />
+
         <button>create event</button>
       </form>
     </div>
@@ -18,10 +49,12 @@
 
 <script>
 import ModalDefault from "@/components/layout/modal/default";
+import Multiselect from "vue-multiselect";
 
 export default {
   components: {
-    ModalDefault
+    ModalDefault,
+    Multiselect
   },
   props: {
     visible: {
@@ -29,20 +62,63 @@ export default {
       default: false
     }
   },
+  computed: {
+    title() {
+      return {
+        id: "title",
+        type: "input",
+        label: "title"
+      };
+    },
+    participantList() {
+      return {
+        id: "participant_list",
+        name: "participantList",
+        type: "multiselect",
+        options: ["one", "two", "three"],
+        label: "participant list"
+      };
+    },
+    facilitatorUser() {
+      return {
+        id: "facilitator_user_id",
+        type: "select",
+        options: ["one", "two", "three"],
+        label: "participant user"
+      };
+    },
+    secretaryUser() {
+      return {
+        id: "secretary_user_id",
+        type: "select",
+        options: ["one", "two", "three"],
+        label: "secretary user"
+      };
+    },
+    start() {
+      return {
+        id: "start",
+        type: "datetimepicker",
+        label: "start"
+      };
+    },
+    end() {
+      return {
+        id: "end",
+        type: "datetimepicker",
+        label: "end"
+      };
+    }
+  },
   data() {
     return {
-      fieldList: [
-        {
-          id: "name",
-          value: "",
-          label: "name"
-        },
-        {
-          id: "surname",
-          value: "",
-          label: "surname"
-        }
-      ]
+      titleValue: "",
+      participantListValue: "",
+      facilitatorUserValue: "",
+      secretaryUserValue: "",
+      startValue: "",
+      endValue: "",
+      status: "scheduled"
     };
   },
   methods: {
@@ -51,13 +127,13 @@ export default {
      * @returns Promise<void>
      */
     async create() {
-      const payload = {
-        name: this.fieldList[0].value,
-        surname: this.fieldList[1].value
-      };
-      await this.$store.dispatch("event/create", payload);
-      this.clearFieldListValues();
-      this.$emit("close");
+      // const payload = {
+      //   name: this.fieldList[0].value,
+      //   surname: this.fieldList[1].value
+      // };
+      // await this.$store.dispatch("event/create", payload);
+      // this.clearFieldListValues();
+      // this.$emit("close");
     },
     /**
      * @returns void
